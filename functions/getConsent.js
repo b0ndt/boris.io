3c20b620a41a98c3a6714b89df15a62c21665645
Boris Fruendt <boris.fruendt@jvm.de>
2018-10-23 14:41:55 +0200

    :chart_with_upwards_trend: Activated tracking

--- functions/getConsent.js
+++ functions/getConsent.js
@@ -1,8 +1,8 @@
 exports.handler = function(event, context, callback) {
     callback(null, {
       statusCode: 200,
-      body: {
+      body: JSON.stringify({
         promptIfUnknown: true
-      }
+      })
     });
   };
