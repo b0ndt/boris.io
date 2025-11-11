4fa74054519be37751aa90fa71cd02587bc82816
Boris Fruendt <boris.fruendt@gmail.com>
2025-11-11 10:06:25 +0100

    Fix AMP validation issues

--- functions/getConsent.js
+++ functions/getConsent.js
@@ -1,6 +1,15 @@
 exports.handler = function(event, context, callback) {
+    const origin = event.headers.origin || event.headers.Origin || '*';
+    
     callback(null, {
       statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': origin,
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Credentials': 'true',
+        'Content-Type': 'application/json'
+      },
       body: JSON.stringify({
         promptIfUnknown: true
       })
